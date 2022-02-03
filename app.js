63ba6b3dfe57a784a2a8af2abf5c2d5f26030726
tchstud21b104 <98838474+tchstud21b104@users.noreply.github.com>
2022-02-03 08:39:58 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -7,179 +7,180 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: "*",
-    },
+  cors: {
+    origin: "*",
+  },
 });
 
 const maxPlayers = 2;
 const roomNameToGameManager = new Map();
 
 io.on("connection", (socket) => {
-    onJoinRoom(socket);
+  onJoinRoom(socket);
 });
 
 httpServer.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 const onDisconnect = (socket, roomName) => {
-    socket.on("disconnect", (reason) => {
-        const gameManager = roomNameToGameManager.get(roomName);
-        socket.broadcast.emit("onClientDisconnect", socket.id);
-        io.disconnectSockets();
-        gameManager.reset();
-    });
+  socket.on("disconnect", (reason) => {
+    const gameManager = roomNameToGameManager.get(roomName);
+    socket.broadcast.emit("onClientDisconnect", socket.id);
+    io.disconnectSockets();
+    gameManager.closeRoom();
+  });
 };
 
 const onJoinRoom = (socket) => {
-    socket.on("onJoinRoom", (roomName) => {
-        let gameManager = roomNameToGameManager.get(roomName);
+  socket.on("onJoinRoom", (roomName) => {
+    let gameManager = roomNameToGameManager.get(roomName);
 
-        if (
-            gameManager &&
-            gameManager.playersAmount >= gameManager.maxPlayers
-        ) {
-            console.log("Room is full, client - ", socket.id, "disconnected");
-            socket.emit("onRoomFull", roomName);
-            socket.disconnect();
-        }
+    if (gameManager && gameManager.playersAmount >= gameManager.maxPlayers) {
+      console.log("Room is full, client - ", socket.id, "disconnected");
+      socket.emit("onRoomFull", roomName);
+      socket.disconnect();
+    }
 
-        if (!gameManager) {
-            roomNameToGameManager.set(
-                roomName,
-                new GameManager(maxPlayers, roomName)
-            );
+    if (!gameManager) {
+      roomNameToGameManager.set(
+        roomName,
+        new GameManager(maxPlayers, roomName)
+      );
 
-            gameManager = roomNameToGameManager.get(roomName);
-        }
+      gameManager = roomNameToGameManager.get(roomName);
+    }
 
-        socket.join(roomName);
-        gameManager.addPlayer(socket);
+    socket.join(roomName);
+    gameManager.addPlayer(socket);
 
-        onGetPlayersName(socket, roomName);
-        onDisconnect(socket, roomName);
-        onCardTakenFromPile(socket, roomName);
-        onCardTakeFromDeck(socket, roomName);
-        onYaniv(socket, roomName);
+    onGetPlayersName(socket, roomName);
+    onDisconnect(socket, roomName);
+    onCardTakenFromPile(socket, roomName);
+    onCardTakeFromDeck(socket, roomName);
+    onYaniv(socket, roomName);
+    onRestartGame(socket, roomName);
 
-        if (gameManager.playersAmount === gameManager.maxPlayers) {
-            setTimeout(() => {
-                onGameStart(socket, roomName);
-            }, 100);
-        }
-    });
+    if (gameManager.playersAmount === gameManager.maxPlayers) {
+      setTimeout(() => {
+        onGameStart(socket, roomName);
+      }, 100);
+    }
+  });
 };
 
 const onGameStart = (socket, roomName) => {
-    const gameManager = roomNameToGameManager.get(roomName);
+  const gameManager = roomNameToGameManager.get(roomName);
 
-    gameManager.initGame();
-    gameManager.players.forEach((player) => {
-        io.to(player.id).emit("onGameStart", player.hand);
-        io.to(player.id).emit(
-            "onPlayersStateChange",
-            gameManager.getPlayersState()
-        );
-        io.to(player.id).emit("onPileUpdate", {
-            pile: gameManager.pile,
-            topPile: gameManager.topPile,
-        });
+  gameManager.initGame();
+  gameManager.players.forEach((player) => {
+    io.to(player.id).emit("onGameStart", player.hand);
+    io.to(player.id).emit(
+      "onPlayersStateChange",
+      gameManager.getPlayersState()
+    );
+    io.to(player.id).emit("onPileUpdate", {
+      pile: gameManager.pile,
+      topPile: gameManager.topPile,
     });
+  });
 
-    changePlayerTurn(gameManager);
+  changePlayerTurn(gameManager);
 };
 
 const onCardTakenFromPile = (socket, roomName) => {
-    /*
+  /*
     2. check if the player has this card, and the pile also has this card (else change turns).
   */
-    socket.on("onCardTakenFromPile", ({ thrownCards, cardFromPile }) => {
-        const gameManager = roomNameToGameManager.get(roomName);
+  socket.on("onCardTakenFromPile", ({ thrownCards, cardFromPile }) => {
+    const gameManager = roomNameToGameManager.get(roomName);
 
-        if (!gameManager.isPlayerTurn(socket.id)) {
-            return;
-        }
+    if (!gameManager.isPlayerTurn(socket.id)) {
+      return;
+    }
 
-        gameManager.takeCardFromPile(socket.id, thrownCards, cardFromPile);
+    gameManager.takeCardFromPile(socket.id, thrownCards, cardFromPile);
 
-        socket.emit("onCardTaken", gameManager.getPlayer(socket.id).hand);
-        io.to(roomName).emit("onPileUpdate", {
-            pile: gameManager.pile,
-            topPile: gameManager.topPile,
-        });
-
-        io.to(roomName).emit(
-            "onPlayersStateChange",
-            gameManager.getPlayersState()
-        );
-        changePlayerTurn(gameManager);
+    socket.emit("onCardTaken", gameManager.getPlayer(socket.id).hand);
+    io.to(roomName).emit("onPileUpdate", {
+      pile: gameManager.pile,
+      topPile: gameManager.topPile,
     });
+
+    io.to(roomName).emit("onPlayersStateChange", gameManager.getPlayersState());
+    changePlayerTurn(gameManager);
+  });
 };
 
 const onCardTakeFromDeck = (socket, roomName) => {
-    /*
+  /*
     2. check if the player has this card, and the deck has cards in it (else change turns).
   */
-    socket.on("onCardTakeFromDeck", (thrownCards) => {
-        const gameManager = roomNameToGameManager.get(roomName);
+  socket.on("onCardTakeFromDeck", (thrownCards) => {
+    const gameManager = roomNameToGameManager.get(roomName);
 
-        if (!gameManager.isPlayerTurn(socket.id)) {
-            return;
-        }
+    if (!gameManager.isPlayerTurn(socket.id)) {
+      return;
+    }
 
-        gameManager.takeCardFromDeck(socket.id, thrownCards);
+    gameManager.takeCardFromDeck(socket.id, thrownCards);
 
-        socket.emit("onCardTaken", gameManager.getPlayer(socket.id).hand);
-        io.to(roomName).emit("onPileUpdate", {
-            pile: gameManager.pile,
-            topPile: gameManager.topPile,
-        });
-
-        io.to(roomName).emit(
-            "onPlayersStateChange",
-            gameManager.getPlayersState()
-        );
-        changePlayerTurn(gameManager);
+    socket.emit("onCardTaken", gameManager.getPlayer(socket.id).hand);
+    io.to(roomName).emit("onPileUpdate", {
+      pile: gameManager.pile,
+      topPile: gameManager.topPile,
     });
+
+    io.to(roomName).emit("onPlayersStateChange", gameManager.getPlayersState());
+    changePlayerTurn(gameManager);
+  });
 };
 
 const onGetPlayersName = (socket, roomName) => {
-    socket.on("onGetPlayerName", (name) => {
-        roomNameToGameManager.get(roomName).getPlayer(socket.id).name = name;
-    });
+  socket.on("onGetPlayerName", (name) => {
+    roomNameToGameManager.get(roomName).getPlayer(socket.id).name = name;
+  });
 };
 
 const onYaniv = (socket, roomName) => {
-    socket.on("onYaniv", (id) => {
-        const gameManager = roomNameToGameManager.get(roomName);
+  socket.on("onYaniv", (id) => {
+    const gameManager = roomNameToGameManager.get(roomName);
 
-        if (gameManager.calcHandSum(id) <= 7) {
-            const minScorePlayer = gameManager.getMinHandScorePlayer();
+    if (gameManager.calcHandSum(id) <= 7) {
+      const minScorePlayer = gameManager.getMinHandScorePlayer();
 
-            if (id === minScorePlayer.id) {
-                io.to(roomName).emit("onPlayerWin", {
-                    winner: minScorePlayer,
-                    state: gameManager.players,
-                });
-            } else {
-                io.to(roomName).emit("onAsaf", {
-                    winner: minScorePlayer,
-                    state: gameManager.players,
-                });
-            }
-        }
-    });
+      if (id === minScorePlayer.id) {
+        io.to(roomName).emit("onPlayerWin", {
+          winner: minScorePlayer,
+          state: gameManager.players,
+        });
+      } else {
+        io.to(roomName).emit("onAssaf", {
+          winner: minScorePlayer,
+          state: gameManager.players,
+        });
+      }
+    }
+  });
+};
+
+const onRestartGame = (socket, roomName) => {
+  socket.on("onRestartGame", () => {
+    const gameManager = roomNameToGameManager.get(roomName);
+    gameManager.restartGame();
+
+    onGameStart(socket, roomName);
+  });
 };
 
 const validateDepositedCards = (cards, roomName) => {
-    console.log("To Be Implemented");
+  console.log("To Be Implemented");
 };
 
 const changePlayerTurn = (gameManager) => {
-    gameManager.changePlayerTurn();
-    io.to(gameManager.roomName).emit(
-        "onTurnChanged",
-        gameManager.currentPlayerTurn
-    );
+  gameManager.changePlayerTurn();
+  io.to(gameManager.roomName).emit(
+    "onTurnChanged",
+    gameManager.currentPlayerTurn
+  );
 };
--- gameManager.js
+++ gameManager.js
@@ -2,116 +2,128 @@ const { cardsToNumber } = require("./utils");
 const { xor } = require("lodash");
 
 class GameManager {
-    constructor(maxPlayers, roomName) {
-        this.deck = new Map(cardsToNumber);
-        this.maxPlayers = maxPlayers;
-        this.playersAmount = 0;
-        this.pile = [];
-        this.topPile = [];
-        this.players = [];
-        this.currentPlayerTurn = "";
-        this.roomName = roomName;
-    }
+  constructor(maxPlayers, roomName) {
+    this.deck = new Map(cardsToNumber);
+    this.maxPlayers = maxPlayers;
+    this.playersAmount = 0;
+    this.pile = [];
+    this.topPile = [];
+    this.players = [];
+    this.currentPlayerTurn = "";
+    this.roomName = roomName;
+  }
 
-    addPlayer(socket) {
-        this.playersAmount += 1;
-        this.players.push({ id: socket.id, hand: [] });
-    }
+  addPlayer(socket) {
+    this.playersAmount += 1;
+    this.players.push({ id: socket.id, hand: [], totalScore: 0 });
+  }
 
-    initGame() {
-        const firstCard = this.getRandomCard();
-        this.pile.push(firstCard);
-        this.topPile.push(firstCard);
+  initGame() {
+    const firstCard = this.getRandomCard();
+    this.pile.push(firstCard);
+    this.topPile.push(firstCard);
 
-        this.players.forEach((player) => {
-            for (let index = 0; index < 5; index++) {
-                player.hand.push(this.getRandomCard());
-            }
-        });
-    }
-
-    isPlayerTurn(id) {
-        return id === this.currentPlayerTurn;
-    }
-
-    getPlayer(id) {
-        return this.players.find((player) => player.id === id);
-    }
-
-    takeCardFromPile(id, thrownCards, cardFromPile) {
-        this.pile.push(thrownCards);
-        this.pile = this.pile.filter((card) => card !== cardFromPile);
-        this.topPile = thrownCards;
-
-        const player = this.getPlayer(id);
-
-        player.hand = xor(player.hand, thrownCards);
-        player.hand.push(cardFromPile);
-    }
-
-    takeCardFromDeck(id, thrownCards) {
-        this.pile.push(thrownCards);
-        this.topPile = thrownCards;
-
-        const player = this.getPlayer(id);
-
-        player.hand = xor(player.hand, thrownCards);
+    this.players.forEach((player) => {
+      for (let index = 0; index < 5; index++) {
         player.hand.push(this.getRandomCard());
-    }
+      }
+    });
+  }
 
-    changePlayerTurn() {
-        let nextPlayerIndex =
-            (this.players.findIndex(
-                    (player) => player.id === this.currentPlayerTurn
-                ) +
-                1) %
-            this.playersAmount;
+  isPlayerTurn(id) {
+    return id === this.currentPlayerTurn;
+  }
 
-        this.currentPlayerTurn = this.currentPlayerTurn ?
-            this.players[nextPlayerIndex].id :
-            this.players[0].id;
-    }
+  getPlayer(id) {
+    return this.players.find((player) => player.id === id);
+  }
 
-    getPlayersState() {
-        return this.players.map((player) => ({
-            id: player.id,
-            name: player.name,
-            cardsAmount: player.hand.length,
-        }));
-    }
+  takeCardFromPile(id, thrownCards, cardFromPile) {
+    this.pile.push(thrownCards);
+    this.pile = this.pile.filter((card) => card !== cardFromPile);
+    this.topPile = thrownCards;
 
-    reset() {
-        this.deck = new Map(cardsToNumber);
-        this.playersAmount = 0;
-        this.pile = [];
-        this.topPile = [];
-        this.players = [];
-        this.currentPlayerTurn = "";
-    }
+    const player = this.getPlayer(id);
 
-    getRandomCard() {
-        let keys = Array.from(this.deck.keys());
-        const key = Math.floor(Math.random() * keys.length);
-        this.deck.delete(keys[key]);
+    player.hand = xor(player.hand, thrownCards);
+    player.hand.push(cardFromPile);
+  }
 
-        return keys[key];
-    }
+  takeCardFromDeck(id, thrownCards) {
+    this.pile.push(thrownCards);
+    this.topPile = thrownCards;
 
-    calcHandSum(playerId) {
-        return this.getPlayer(playerId).hand.reduce(
-            (total, id) => total + cardsToNumber.get(id),
-            0
-        );
-    }
+    const player = this.getPlayer(id);
 
-    getMinHandScorePlayer() {
-        return this.players.reduce((prevPlayer, currPlayer) => {
-            return this.calcHandSum(prevPlayer.id) <
-                this.calcHandSum(currPlayer.id) ?
-                prevPlayer :
-                currPlayer;
-        });
-    }
+    player.hand = xor(player.hand, thrownCards);
+    player.hand.push(this.getRandomCard());
+  }
+
+  changePlayerTurn() {
+    let nextPlayerIndex =
+      (this.players.findIndex(
+        (player) => player.id === this.currentPlayerTurn
+      ) +
+        1) %
+      this.playersAmount;
+
+    this.currentPlayerTurn = this.currentPlayerTurn
+      ? this.players[nextPlayerIndex].id
+      : this.players[0].id;
+  }
+
+  getPlayersState() {
+    return this.players.map((player) => ({
+      id: player.id,
+      name: player.name,
+      cardsAmount: player.hand.length,
+      totalScore: player.totalScore,
+    }));
+  }
+
+  closeRoom() {
+    this.deck = new Map(cardsToNumber);
+    this.playersAmount = 0;
+    this.pile = [];
+    this.topPile = [];
+    this.players = [];
+    this.currentPlayerTurn = "";
+  }
+
+  restartGame() {
+    this.deck = new Map(cardsToNumber);
+    this.pile = [];
+    this.topPile = [];
+    this.currentPlayerTurn = "";
+    this.players.forEach((player) => {
+      const totalScore = this.calcHandSum(player.id) + player.totalScore;
+      player.hand = [];
+      player.totalScore = totalScore;
+    });
+  }
+
+  getRandomCard() {
+    let keys = Array.from(this.deck.keys());
+    const key = Math.floor(Math.random() * keys.length);
+    this.deck.delete(keys[key]);
+
+    return keys[key];
+  }
+
+  calcHandSum(playerId) {
+    return this.getPlayer(playerId).hand.reduce(
+      (total, id) => total + cardsToNumber.get(id),
+      0
+    );
+  }
+
+  getMinHandScorePlayer() {
+    return this.players.reduce((prevPlayer, currPlayer) => {
+      return this.calcHandSum(prevPlayer.id) < this.calcHandSum(currPlayer.id)
+        ? prevPlayer
+        : currPlayer;
+    });
+  }
 }
 
 module.exports = { GameManager };
